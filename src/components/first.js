295b8ba667e910e6fa4d1b574c1d9c2e660df217
VolodumurGrushka <gru.v@i.ua>
2022-10-07 02:53:47 +0300

    first is ready

---
+++ src/components/first.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function Checkout() {
+	useEffect(props.flushCartHandler());
+
+	return (
+		<div className="page-body">
+			<div className="page-element">
+				<div className="checkout-page">
+					<h2>Successful!</h2>
+
+					<CommonLinkBtn linkTo={"/"} linkName={"Back to shop"} />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Checkout;
